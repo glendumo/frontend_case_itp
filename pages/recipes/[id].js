5cd7aaf1c43cfbddd353bcbe4ac058922df17ad3
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-11-28 11:52:11 +0100

    Added detail page for recipes

---
+++ pages/recipes/[id].js
@@ -0,0 +1,13 @@
+function RecipeDetail() {
+    return (
+        <main className="app-main">
+            <div className="page page--recipe-detail">
+                <div className="container">
+                    <h1>Recipe Detail</h1>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default RecipeDetail;
