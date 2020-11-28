7abad739cc76e2a361afbd715b7d4785a784a68b
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-11-28 11:54:21 +0100

    Added Header & Footer to every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { Header, Footer } from "../components/layout";
+import "../styles/app.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div className="app-body">
+            <Header />
+            <Component {...pageProps} />
+            <Footer />
+        </div>
+    );
 }
 
-export default MyApp
+export default MyApp;
