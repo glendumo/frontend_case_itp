55b2306e3dd25b66ac93844ad3e1dcc94d17e876
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-11-28 11:50:10 +0100

    Added Header & Footer

---
+++ components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = ({ children }) => {
+    return (
+        <footer className="app-footer">
+            <div className="legal">
+                <span>©2020</span>
+                <span>Made by Glenn Dumoulin</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ components/layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import * as Routes from "../../routes";
+
+const Header = ({ children }) => {
+    return (
+        <header className="app-header">
+            <nav className="navigation">
+                <a href={Routes.LANDING} className="logo">
+                    RecipeRoom
+                </a>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
